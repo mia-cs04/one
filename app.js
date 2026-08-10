41b4adc9ed22f5d2105043ebea8cf7e02e7e5b0f
mia-cs04 <miacairns22@gmail.com>
2026-08-10 13:27:56 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // REPLACE WITH YOUR ACTUAL SUPABASE URL AND ANON KEY
-const SUPABASE_URL = "https://eajlzqbleznhyoxnkvwf.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhamx6cWJsZXpuaHlveG5rdndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NzMzNDQsImV4cCI6MjEwMTQ0OTM0NH0._Rnk8WHn5J0-BnCsVtgLy8olAx9o7LpplybzKCTHuws";
+const SUPABASE_URL = "https://upzvsezoslepazltbvch.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwenZzZXpvc2xlcGF6bHRidmNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzMjcwODAsImV4cCI6MjEwMTkwMzA4MH0.sglB_9p36b6ZlZiDWZw_YBNJRRprpqgPeQRdcPxctOA";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const STEALTH_URL = "https://coneqt-s.mountcarmel.tas.edu.au:4430/#?page=/welcome";
